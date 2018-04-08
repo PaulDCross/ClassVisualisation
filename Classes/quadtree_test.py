d9adced23367dd40c59ae9b4c2717a207ba8d557
Cross <paulcross@f2s.com>
2018-04-08 11:54:25 +0100

    formatting

--- Classes/quadtree_test.py
+++ Classes/quadtree_test.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     game_exit = False
     clock = pygame.time.Clock()
-    fps = 30
+    fps = 100
     one_pressed = False
     three_pressed = False
     state = True
@@ -28,10 +28,10 @@ if __name__ == "__main__":
     rectangle = None
     points_in_range = []
     points = []
-    for i in range(1000):
-        p = qtr.Point(random.gauss(dd[0]/2, dd[0]/8), random.gauss(dd[1]/2, dd[1]/8))
-        quadtree.insert(p)
-        points.append(p)
+    # for i in range(1000):
+    #     p = qtr.Point(random.gauss(dd[0]/2, dd[0]/8), random.gauss(dd[1]/2, dd[1]/8))
+    #     quadtree.insert(p)
+    #     points.append(p)
     # for j in range((dd[0]/2)-10, (dd[0]/2)+10):
     #     for k in range((dd[1]/2)-10, (dd[1]/2)+10):
     #         p = qtr.Point(j, k)
@@ -40,12 +40,12 @@ if __name__ == "__main__":
 
     while not game_exit:
         # boundary = qtr.Boundary(0, 0, dd[0], dd[1])
-        # quadtree = qtr.QuadTree(boundary, 4)
-        # points = []
-        # for i in range(1000):
-        #     p = qtr.Point(random.gauss(dd[0]/2, dd[0]/8), random.gauss(dd[1]/2, dd[1]/8))
-        #     quadtree.insert(p)
-        #     points.append(p)
+        quadtree = qtr.QuadTree(boundary, 4)
+        points = []
+        for i in range(1000):
+            p = qtr.Point(random.gauss(dd[0]/2, dd[0]/8), random.gauss(dd[1]/2, dd[1]/8))
+            quadtree.insert(p)
+            points.append(p)
         game_display.fill(WHITE)
         quadtree.show(game_display)
         for event in pygame.event.get():
@@ -70,25 +70,24 @@ if __name__ == "__main__":
             height = 100
             rectangle = qtr.Boundary(target[0] - width/2, target[1] - height/2, target[0] + width/2, target[1] + height/2)
             points_in_range = quadtree.query_range(rectangle)
+            pygame.draw.rect(game_display, GREEN, (rectangle.x1, rectangle.y1, rectangle.x2 - rectangle.x1, rectangle.y2 - rectangle.y1), 1)
+            for point in points_in_range:
+                pygame.draw.circle(game_display, GREEN, (int(point.x), int(point.y)), 2, 0)
+
         if three_pressed:
             p = qtr.Point(target[0], target[1])
             quadtree.insert(p)
+            points.append(p)
             three_pressed = False
-
-        if rectangle is not None:
-            pygame.draw.rect(game_display, (0, 255, 0), (rectangle.x1, rectangle.y1, rectangle.x2 - rectangle.x1, rectangle.y2 - rectangle.y1), 1)
-
-        for point in points_in_range:
-            pygame.draw.circle(game_display, (0, 255, 0), (int(point.x), int(point.y)), 2, 0)
 
         for point in points:
             if point not in points_in_range:
-                pygame.draw.circle(game_display, (0, 0, 0), (int(point.x), int(point.y)), 2, 0)
+                pygame.draw.circle(game_display, BLACK, (int(point.x), int(point.y)), 2, 0)
 
         pygame.display.update()
         clock.tick(fps)
         counter += 1
         # print "Frame: {}".format(counter)
-        # print clock.get_fps()
+        print clock.get_fps()
     pygame.quit()
     quit()
